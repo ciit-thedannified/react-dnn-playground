4ade8dab47b37f02d07587664c84ddee1e0725b0
Danne Uriel Boiser <danne.boiser@ciit.edu.ph>
2024-06-15 13:14:11 +0800

    Final adjustments to App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import './css/App.css'
+import Content from "./components/Content.jsx";
+import {useState} from "react";
+import { Accounts } from "./db/accounts.js";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    let [loggedIn, setLoggedIn] = useState(false);
+
+    function handleAuthLogin(event) {
+        event.preventDefault();
+
+        let username = document.getElementById("username").value;
+        let password = document.getElementById("password").value;
+
+        let fetchedUser = Accounts.get(username);
+
+        if (!fetchedUser) {
+            return alert("User account does not exist");
+        } else {
+            if (password !== fetchedUser.getPassword()) {
+                return alert("Invalid credentials");
+            } else {
+                return setLoggedIn(true);
+            }
+        }
+
+    }
+
+    return (
+        <>
+            <div>
+                {/* Log in interface */}
+                {
+                    !loggedIn &&
+                    (
+                        <>
+                            <div className="login-form">
+                                <h1> Log in </h1>
+                                <form>
+                                    <p className="form-label"> Username: </p>
+                                    <input id="username" type="text" placeholder="Enter username"/>
+                                    <p className="form-label"> Password: </p>
+                                    <input id="password" type="password" placeholder="Enter username"/>
+                                    <br/> <br/>
+                                    <input type="button" value="Submit" onClick={e => handleAuthLogin(e)}/>
+                                </form>
+                            </div>
+                        </>
+                    )
+                }
+
+                {
+                    loggedIn &&
+                    <Content />
+                }
+            </div>
+        </>
+    )
 }
 
 export default App
